cfec9a721299515bc49c9317dcedb527e8f6ec77
Maksim Denisov <denisov.maksim.work@gmail.com>
2023-01-03 17:26:54 +0700

    Slider

--- HH_dashboard.py
+++ HH_dashboard.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
+
+    st.header('Dataframes in the dashboard')
     df = pd.DataFrame({
      'first column': [1, 2, 3, 4],
      'second column': [10, 20, 30, 40]
      })
     st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+    st.header('Sliders')
+    age = st.slider('How old are you?', 0, 130, 50)
+    st.write('I\'m' , age, 'years old')
 
