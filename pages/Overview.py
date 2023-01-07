3c0caf9069787a8be5f882784c370117db207289
Maksim Denisov <denisov.maksim.work@gmail.com>
2023-01-07 19:57:19 +0700

    Added two new pages, new plots and dataframes

---
+++ pages/Overview.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import json
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+st.set_page_config(page_title="Vacancies overview", page_icon="📈")
+
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
+
+# Function to extract data fron .json file
+def extract_json_file(file_name):
+    f = open(file_name, mode='r', encoding='utf8')
+    return json.loads('[' + f.read() + ']')
+
+# Extracting data
+data = extract_json_file('vacancies_data.json')
+df_data = pd.DataFrame(data)
+
+# Constructing plot
+area_jobs = df_data['area-name'].value_counts()
+others = pd.Series(area_jobs[area_jobs.values < vac_num_choice].values.sum(),
+                   index = ['Другие'])
+area_jobs = area_jobs[area_jobs.values > vac_num_choice].append(others)
+
+fig, ax = plt.subplots(figsize=(15,5))
+hist1 = ax.bar(area_jobs.index, area_jobs.values)
+ax.tick_params(axis='x', rotation=90, labelsize=12)
+ax.tick_params(axis='y', labelsize=12)
+ax.set_xlabel('City', fontsize=14)
+ax.set_ylabel('Vacancies number', fontsize=13)
+ax.bar_label(hist1)
+plt.show()
+
+# Adding interactive filter
+vac_num_choice = st.sidebar.slider(
+                    'Max vacansies number:', min_value=area_jobs.min(), 
+                                             max_value=area_jobs.max(), 
+                                             step=1, value=10)
---
+++ pages/Salary.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import json
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+st.set_page_config(page_title="Salaries analysis", page_icon="📈")
+
+# Function to extract data fron .json file
+def extract_json_file(file_name):
+    f = open(file_name, mode='r', encoding='utf8')
+    return json.loads('[' + f.read() + ']')
+
+# Extracting data
+data = extract_json_file('vacancies_data.json')
+df_data = pd.DataFrame(data)
+
+# Adding interactive filters
+job_counts_by_area = df_data['area-name'].value_counts()
+city_choice = st.sidebar.multiselect(
+    'Choose city:', 
+    job_counts_by_area[job_counts_by_area.values > 10].keys(), 
+    default='Москва')
+currency_choice = st.sidebar.multiselect(
+    'Choose currency:', 
+    df_data['salary-currency'].dropna().unique(), 
+    default='RUR')
+
+# Constructing plots
+fig, ax = plt.subplots(figsize=(15,5))
+c1, c2, c3 = sns.color_palette("Set1", 3)
+ax = sns.histplot(df_data['salary-from'][(df_data['area-name'] == city_choice) & 
+                                   (df_data['salary-currency'] == currency_choice)],
+            label = 'Salary From',
+            color = c1,
+            binwidth=30000)
+ax = sns.histplot(df_data['salary-to'][(df_data['area-name'] == city_choice) & 
+                                 (df_data['salary-currency'] == currency_choice)],
+            label = 'Salary To',
+            color = c2,
+            binwidth=30000)
+
+ax.set_xlabel('Salary')
+ax.set_title('Salary for ' + city_choice + ' city')
+ax.legend()
+
+# Showing statistics
+salary_from_stats = df_data['salary-from'][(df_data['area-name'] == city_choice) & 
+                        (df_data['salary-currency'] == currency_choice)].describe().to_frame()
+salary_to_stats = df_data['salary-to'][(df_data['area-name'] == city_choice) & 
+                    (df_data['salary-currency'] == currency_choice)].describe().to_frame()
+
+col1, col2 = st.columns(2)
+    
+with col1:
+    st.write("Statistics for Salary-From")
+    st.dataframe(salary_from_stats)
+with col2:
+    st.write("Statistics for Salary-To")
+    st.dataframe(salary_to_stats)
+
+
+
